b7681e3d7907bfe19db2bc9136d8a2c0a3e99d63
Clement Garin <clegopod@hotmail.fr>
2020-05-31 21:18:08 +0200

    hello

--- src/hello.java
+++ src/hello.java
@@ -3,19 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test_31.pkg05;
 
 /**
  *
  * @author BTS sio
  */
-public class Test_3105 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-      System.out.println("argh");
-    }
+public class hello {
     
 }
+
+
+//cooucou
